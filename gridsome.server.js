0b97e09e8d21149d7c56d7b70f6a13e8568ea233
k <kunkunzzzz@outlook.com>
2020-11-19 11:09:26 +0800

    feat: 完成社交圈功能

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,9 +5,62 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const axios = require('axios')
+const githubName = 'cl-k'
+
+async function getFollowings (actions) {
+  const collection = actions.addCollection('following')
+  const { data } = await axios.get(`https://api.github.com/users/${githubName}/following`)
+  if (data) {
+    for (const item of data) {
+      collection.addNode(item)
+    }
+  }
+}
+
+async function getFollowers (actions) {
+  const collection = actions.addCollection('followers')
+  let { data } = await axios.get(`https://api.github.com/users/${githubName}/followers`)
+  if (!data || !data.length) {
+    data = [{
+      avatar_url: '',
+      html_url: '',
+      login: '',
+      id: '-1',
+    }]
+  }
+  if (data) {
+    for (const item of data) {
+      collection.addNode(item)
+    }
+  }
+}
+
+async function getUser (actions) {
+  const collection = actions.addCollection('user')
+  const { data } = await axios.get(`https://api.github.com/users/${githubName}`)
+  if (data) {
+    collection.addNode(data)
+  }
+}
+
+async function getProjects (actions) {
+  const collection = actions.addCollection('project')
+  const { data } = await axios.get(`https://api.github.com/users/${githubName}/repos`)
+  if (data && data) {
+    for (const item of data) {
+      collection.addNode(item)
+    }
+  }
+}
+
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
+  api.loadSource(async (actions) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+    await getFollowings(actions)
+    await getFollowers(actions)
+    await getUser(actions)
+    await getProjects(actions)
   })
 
   api.createPages(({ createPage }) => {
